dce28b8c67fc123e448ffaed74efbe63b0f0e5ea
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-04-18 15:03:48 +0300

    update packges

---
+++ src/pages/main.jsx
@@ -0,0 +1,15 @@
+import react, {Component} from 'react'
+
+
+class Main extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <main>
+                <h2> this is main page </h2>
+            </main>
+         );
+    }
+}
+ 
+export default Main;
